d7d2d6907bbe93b7e5202760c1d658d9ac4410cb
davit dudashvili <dudaahvilid@gmail.com>
2022-05-26 13:54:45 +0400

    woodbad

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$('.firstsldier').owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    merge:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+    // responsive:{
+    //     678:{
+    //         mergeFit:true
+    //     },
+    //     1000:{
+    //         mergeFit:false
+    //     }
+    // }
+});
+$('.secondslider').owlCarousel({
+    items:3,
+    loop:true,
+    margin:104,
+    merge:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+    // responsive:{
+    //     678:{
+    //         mergeFit:true
+    //     },
+    //     1000:{
+    //         mergeFit:false
+    //     }
+    // }
+});
