0784a527627fc60d20b3ec506c5c0736c47228ad
vincxa <dudaahvilid@gmail.com>
2022-05-27 17:52:33 +0400

    mexute ganaxleba idialurze odnav uketesi yvelaferi winaris

--- js/main.js
+++ js/main.js
@@ -3,34 +3,20 @@ $('.firstsldier').owlCarousel({
     loop:true,
     margin:10,
     merge:true,
-    // autoplay:true,
-    // autoplayTimeout:2000,
-    // autoplayHoverPause:true
-    // responsive:{
-    //     678:{
-    //         mergeFit:true
-    //     },
-    //     1000:{
-    //         mergeFit:false
-    //     }
-    // }
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+    
 });
 $('.secondslider').owlCarousel({
     items:3,
     loop:true,
     margin:104,
     merge:true,
-    // autoplay:true,
-    // autoplayTimeout:2000,
-    // autoplayHoverPause:true
-    // responsive:{
-    //     678:{
-    //         mergeFit:true
-    //     },
-    //     1000:{
-    //         mergeFit:false
-    //     }
-    // }
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+    
 });
 
 function flipone(){
